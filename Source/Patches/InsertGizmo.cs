b48482a40c887c8119e6071c8f146add9cbfc523
Flia <miracleshift@gmail.com>
2025-11-13 12:36:22 +0100

    Updated the icon. NOW it's crispy.

---
+++ Source/Patches/InsertGizmo.cs
@@ -0,0 +1,15 @@
+using RimWorld;
+using Verse;
+
+namespace RT_Saltwater;
+
+[StaticConstructorOnStartup]
+public class InsertGizmo
+{
+    static InsertGizmo()
+    {
+        /*var i = DesignationCategoryDefOf.Zone.specialDesignatorClasses.IndexOf(typeof(Designator_ZoneAdd_Growing));
+        DesignationCategoryDefOf.Zone.specialDesignatorClasses.Add(typeof(Designator_ZoneAdd_Saltwater));
+        Log.Message(i + " success?");*/
+    }
+}
