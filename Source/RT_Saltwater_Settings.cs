6da055f477eee36628de3ab7d111e051ef4d6acb
Flia <miracleshift@gmail.com>
2025-11-19 12:36:27 +0100

    Minor fixes + attempt to clean up compiler info

--- Source/RT_Saltwater_Settings.cs
+++ Source/RT_Saltwater_Settings.cs
@@ -17,10 +17,31 @@ public class RT_Saltwater_Settings : ModSettings
 
     public static void DoSettingsWindowContents(Rect inRect)
     {
-        var listingStandard = new Listing_Standard();
+        Listing_Standard listingStandard = new Listing_Standard();
         listingStandard.Begin(inRect);
         listingStandard.CheckboxLabeled("RT_Saltwater_MarineAgricultureLabel".Translate(), ref MarineAgriculture, "RT_Saltwater_MarineAgricultureTooltip".Translate());
         listingStandard.CheckboxLabeled("RT_Saltwater_IndustrialRunoffTooLabel".Translate(), ref IndustrialRunoffToo, "RT_Saltwater_IndustrialRunoffTooTooltip".Translate());
         listingStandard.End();
+    }
+}
+
+public class RT_Saltwater : Mod
+{
+    private readonly RT_Saltwater_Settings settings;
+
+    public RT_Saltwater(ModContentPack content) : base(content)
+    {
+        settings = GetSettings<RT_Saltwater_Settings>();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        RT_Saltwater_Settings.DoSettingsWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "RT_Saltwater_Settings".Translate();
     }
 }
